4c1255d240d358dc0c1d90641c221bc126cf1335
AngwenyiOgata <147700772+AngwenyiOgata@users.noreply.github.com>
2023-12-23 18:39:31 +0300

    Add files via upload

--- JS/Script.js
+++ JS/Script.js
@@ -1,1 +1,18 @@
+(function () {
+  'use strict';
 
+  const hamburger = document.querySelector('.icon-hamburger');
+  const navigation = document.querySelector('.app__header-nav');
+
+  hamburger.addEventListener('click', (evt)=>{
+    evt.preventDefault();
+  if (navigation.style.display === 'none') {
+    navigation.style.display = 'flex';
+    hamburger.src = './images/icon-close.svg';
+  } else {
+    navigation.style.display = 'none';
+    hamburger.src = './images/icon-hamburger.svg';
+  }
+
+  })
+}())
